e1f18d17a7183f1981e59b88f6a9ade38f953364
chansen424 <chansen424@yahoo.com>
2019-10-10 16:27:32 -0400

    Added Icons
    Adjusted alignment

--- learning2.0/src/index.js
+++ learning2.0/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Computer from './monitor.png'
+import Book from './book.png'
 import './Hook.css'
 
 const Summary = (props) => {
   return (
-    <p className="summary">I am a curious man.</p>
+    <p className="summary">Computer Science <a href="https://cornell.edu">@Cornell</a></p>
   )
 }
 
@@ -12,12 +14,17 @@ const Left = (props) => {
   return (
     <div className="split left">
       <div className="centered">
+        <div className="leftLine">
+        <img alt="Computer" src={Computer} />
+        <img alt="Book" src={Book} />
         <h1 className="hook">
-          Developer.
-          <br />
-          Bookworm.
-        </h1>
-        <Summary />
+            Developer.
+            <br />
+            Bookworm.
+          </h1>
+          <Summary />
+          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/good-ware" title="Freepik">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </div>
       </div>
     </div>
   )
@@ -27,7 +34,6 @@ const Right = (props) => {
   return (
     <div className="split right">
       <div className="centered">
-        
       </div>
     </div>
   )
