d7704dc901e5c737332f0c258dd934defa5f25b3
chansen424 <chansen424@yahoo.com>
2019-10-10 17:28:33 -0400

    Icons & Summary
    Links to Experience, Icons and Styling

--- learning2.0/src/index.js
+++ learning2.0/src/index.js
@@ -2,11 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Computer from './monitor.png'
 import Book from './book.png'
+import LinkedIn from './linkedin.png'
+import GitHub from './github.png'
+import Email from './mail.png'
 import './Hook.css'
+
+const Experience = ({go="https://cornell.edu", text="Cornell"}) => {
+  return (
+    <a className="expLink" href={go}>@{text}</a>
+  )
+}
+
+const Link = ({go="https://github.com/chansen424", icon=GitHub}) => {
+  return (
+    <a href={go}><img alt="Book" src={icon}/></a>
+  )
+}
 
 const Summary = (props) => {
   return (
-    <p className="summary">Computer Science <a href="https://cornell.edu">@Cornell</a></p>
+    <div>
+      <p className="summary">
+        Currently developing <Experience go="https://cornelldti.org" text="CornellDTI"/>. 
+        Formerly Full Stack Intern <Experience go="https://startuptree.co" text="StartupTree" />. 
+        Check me out below.
+      </p>
+      <Link />
+      <Link go="https://linkedin.com/in/christopherphansen" icon={LinkedIn} />
+      <Link go="mailto:cph64@cornell.edu?subject=Website Inquiry" icon={Email}/>
+    </div>
   )
 }
 
@@ -18,12 +42,12 @@ const Left = (props) => {
         <img alt="Computer" src={Computer} />
         <img alt="Book" src={Book} />
         <h1 className="hook">
-            Developer.
+            Developer
             <br />
-            Bookworm.
+            & Bookworm.
           </h1>
           <Summary />
-          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/good-ware" title="Freepik">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          {/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/authors/good-ware" title="Freepik">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
         </div>
       </div>
     </div>
